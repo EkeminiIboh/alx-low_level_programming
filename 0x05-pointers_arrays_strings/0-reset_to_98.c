c27d85be4143ebd210c0a4eadbe8a13250aa89fc
EkeminiIboh <kemsyiboh2@gmail.com>
2023-07-24 15:36:45 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 = a function that takes pointer to an int a s parameter
+ * reset_to_98 - a function that takes pointer to an int a s parameter
  * and update the value it points to to 98
  * @n: input
  * Return: n
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
+#include "main.h"
 
+/**
+ * swap_int - swaps the value of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return nothing on success
+ */
+void swap_int(int *a, int *b);
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
